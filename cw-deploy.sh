da1ea94089e864130dfde5f7d86fb5830bb511c5
jody <thisjody@gmail.com>
2023-07-11 16:41:08 -0700

    reverting from workflow

--- cw-deploy.sh
+++
@@ -1,5 +0,0 @@
-gcloud workflows deploy sw-cw-cf-gcs-bq \
-  --source=cw.json \
-  --service-account=untar-ingest@acep-ext-eielson-2021.iam.gserviceaccount.com \
-  --location=us-west1
-
--- cw-subscription.sh
+++
@@ -1,4 +0,0 @@
-gcloud pubsub subscriptions create sw-cw-cf-gcs-bq-subscription \
-  --topic=sw-df-cf-bq-ingest \
-  --push-endpoint="https://workflowexecutions.googleapis.com/v1/projects/acep-ext-eielson-2021/locations/us-west1/workflows/sw-cw-cf-gcs-bq/executions" \
-  --push-auth-service-account=untar-ingest@acep-ext-eielson-2021.iam.gserviceaccount.com
