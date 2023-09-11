ed3971a2ffa8035cc73d7fbb02ad80db4cef746c
jody <thisjody@gmail.com>
2023-09-11 16:15:39 -0700

    convert to .env

--- eiedeploy.sh
+++ eiedeploy.sh
@@ -1,10 +1,16 @@
+#!/bin/bash
+
+# Source the .env file
+source eiedeploy.env
+
+# Deploy the function
 gcloud functions deploy sw-cf-gcs-ps-bq \
-  --gen2 \
-  --runtime=python311 \
-  --region=us-west1 \
-  --source=src \
-  --entry-point=bq_load_from_gcs \
-  --memory 16384MB \
-  --timeout 540s  \
-  --trigger-topic sw-df-cf-bq-ingest \
-  --set-env-vars PUBSUB_TOPIC=sw-cf-bq-pp-dt-rs
+  --$GEN2 \
+  --runtime=$RUNTIME \
+  --region=$REGION \
+  --source=$SOURCE \
+  --entry-point=$ENTRY_POINT \
+  --memory=$MEMORY \
+  --timeout=$TIMEOUT \
+  --trigger-topic=$TRIGGER_TOPIC \
+  --set-env-vars PUBSUB_TOPIC=$PUBSUB_TOPIC
